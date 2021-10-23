43871bcb10b3496a92a4ae79473af1692d0efb70
aaaasif <alexasif4@gmail.com>
2021-10-24 00:37:09 +0600

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,12 @@
+console.log(process.env);
+const firebaseConfig = {
+    apiKey: "AIzaSyCInr-xQzZxziEYTfRaae7OdCRndk1yWds",
+    authDomain: "genius-car-mechanics-329a0.firebaseapp.com",
+    projectId: "genius-car-mechanics-329a0",
+    storageBucket: "genius-car-mechanics-329a0.appspot.com",
+    messagingSenderId: "555780776209",
+    appId: "1:555780776209:web:da763bdb03c3936ef16cb0",
+    measurementId: "G-DYLK63VPYH"
+};
+
+export default firebaseConfig;
